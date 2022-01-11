f1bf3ab603e35f2b2801cfd7bf67a38c943198cf
Jasper de Laat <jasper.de.laat@live.nl>
2022-01-11 22:22:46 +0100

    WIP: - Create SiriusStringUtils plugin. - Create SiriusStringUtilsEditor module.

---
+++ Plugins/SiriusStringUtils/Source/SiriusStringUtilsEditor/Private/SiriusStringUtilsEditor.cpp
@@ -0,0 +1,5 @@
+// Copyright 2022-2022 Jasper de Laat. All Rights Reserved.
+
+#include "SiriusStringUtilsEditor.h"
+
+IMPLEMENT_MODULE(FSiriusStringUtilsEditorModule, SiriusStringUtilsEditor)
---
+++ Plugins/SiriusStringUtils/Source/SiriusStringUtilsEditor/Public/SiriusStringUtilsEditor.h
@@ -0,0 +1,9 @@
+// Copyright 2022-2022 Jasper de Laat. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+class FSiriusStringUtilsEditorModule final : public IModuleInterface
+{
+};
