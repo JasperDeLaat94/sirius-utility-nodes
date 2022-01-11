e29154f6596001166556d65604cb518deeef2893
Jasper de Laat <jasper.de.laat@live.nl>
2022-01-11 18:42:27 +0100

    Add blank UE4.27 project.

---
+++ Source/PluginDev/PluginDevGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PluginDevGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PluginDev_API APluginDevGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
